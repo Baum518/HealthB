30130f063e5c5d703aa896746d2a13f640828ab5
Baum518 <baum_1st@163.com>
2016-07-03 13:58:43 +0800

    Baum

--- HealthB/src/main/java/me/groupb/service/UserService.java
+++ HealthB/src/main/java/me/groupb/service/UserService.java
@@ -5,18 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import me.groupb.dao.UserDao;
 import me.groupb.model.User;
 
 
-@Service("userService")
+@Service
 @Transactional
 public class UserService {
 	private static final String User = null;
 	@Autowired
 	private UserDao userDao;
 	
+	
+	@RequestMapping("findUser")
 	public List<User> findUser(){
 		List<User> lUsers = userDao.listHql(User);
 		return lUsers;
