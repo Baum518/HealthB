09953351def78dcb8ec0b3f674e1bdbd65c8ceba
Baum518 <baum_1st@163.com>
2016-07-04 08:13:18 +0800

    Baum

--- HealthB/src/test/java/me/groupb/service/UserServiceTest.java
+++ HealthB/src/test/java/me/groupb/service/UserServiceTest.java
@@ -3,12 +3,10 @@ package me.groupb.service;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.util.List;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import me.groupb.model.User;
 import me.groupb.util.SpringTestBase;
 
 public class UserServiceTest extends SpringTestBase{
@@ -22,11 +20,7 @@ public class UserServiceTest extends SpringTestBase{
 	}
 	
 	@Test
-	public void testfindUser(){
-		List<User> lUsers = userService.findUser();
-		for(User user:lUsers){
-			System.out.println("NikeName:" + user.getNickName());
-		}
+	public void testFindUser(){
 		assertTrue(userService.findUser().size()!=0);
 	}
 }
